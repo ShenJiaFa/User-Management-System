17d924b31b4a6d86736c24c37ae2b69711297560
ShenJiaFa <sjfdzh@gmail.com>
2022-08-05 22:45:56 +0800

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Function:
+ *
+ * @author ShenJiaFa
+ * @since 2022/8/5
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
